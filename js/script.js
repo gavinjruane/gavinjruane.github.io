54d799a7f3806730e00306bb870816d9a9dfa930
gavinjruane <54756142+gavinjruane@users.noreply.github.com>
2020-07-16 01:18:29 -0700

    new recipes

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const message = "Welcome to Recipe Book! What is your name?";
+// let user = prompt(message);
+// let pageTitle = `${user}'s Recipe Book`
+
+// console.log(user);
+// document.querySelector("#heading").innerHTML = pageTitle;
